4d5015c7374324f037c4e9a580fa71852892a539
Robert Lissi <96295395+Robert9789@users.noreply.github.com>
2022-12-15 06:32:10 -0800

    Delete notifications directory

--- notifications/api/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls import url
-
-from .views import NotificationListAPIView
-
-urlpatterns = [
-    url(r'^notifications/$', NotificationListAPIView.as_view(), name='list_notifications'),
-]
