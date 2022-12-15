e0fdeaae8bf40d37a479a658bbdf58c126982daf
Robert Lissi <96295395+Robert9789@users.noreply.github.com>
2022-12-15 06:31:39 -0800

    Delete reports directory

--- reports/api/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from django.conf.urls import url
-
-from .views import ReportListCreateAPIView
-
-urlpatterns = [
-    url(r'^reports/$', ReportListCreateAPIView.as_view(), name='list_or_create_reports'),
-]
