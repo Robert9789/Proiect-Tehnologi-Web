f29270acf1f2a87f704f0de74011c80ee4116503
Robert Lissi <96295395+Robert9789@users.noreply.github.com>
2022-12-15 06:30:59 -0800

    Delete comments directory

--- comments/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import CommentDestroyAPIView, CommentListCreateAPIView
-
-urlpatterns = [
-    url(r'^comments/$', CommentListCreateAPIView.as_view(), name='list_or_create_comments'),
-    url(r'^comments/(?P<id>\d+)/$', CommentDestroyAPIView.as_view(), name='destroy_comments'),
-]
